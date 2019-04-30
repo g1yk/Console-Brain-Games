706e386f16e19d03be0bdb5cab8b02df5066d5d9
Vitalii <vitaly.agapov@Outlook.com>
2019-04-30 17:07:33 -0500

    added .travis.yml

--- Makefile
+++ Makefile
@@ -1,3 +1,16 @@
+
 install:
 	npm install
+start:
+	sudo npx babel-node src/bin/brain-games.js
+build:
+	rm -rf dist
+	npm run build
 publish:
+	npm publish
+lint:
+	npx eslint .
+test:
+	npm test
+test-coverage:
+	npm test -- --coverage
--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ export const greeting = (name) => {
     console.log('Welcome to the Brain Games!')
 
 const userName = readlineSync.question('May I have your name? ');
-console.log('Hello ' + userName + '!');
+console.log('Hello, ' + userName + '!');
 }
 
