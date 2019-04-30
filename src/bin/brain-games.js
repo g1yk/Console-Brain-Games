9c70d0b17cc418bafb5f2734ddfc4e19d2672e0d
Vitalii <vitaly.agapov@Outlook.com>
2019-04-30 16:18:31 -0500

    added greeting

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import {greeting} from '../index';
+
+
+
+
+greeting()
+
+
---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+import readlineSync from 'readline-sync';
+
+
+
+export const greeting = (name) => {
+
+    console.log('Welcome to the Brain Games!')
+
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hello ' + userName + '!');
+}
+
