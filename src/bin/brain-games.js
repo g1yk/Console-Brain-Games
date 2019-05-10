d09ee29dd4167a33c51a64098f7f46f808a8590f
Vitalii <vitaly.agapov@Outlook.com>
2019-05-09 19:05:41 -0500

    core done, even done

--- src/bin/brain-games.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import {greeting, question} from '../index';
-
-
-greeting();
-export const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
-question();
