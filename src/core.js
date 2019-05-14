aa54481bcb767541cd73c77844c73c96d38e07b0
Vitalii <vitaly.agapov@Outlook.com>
2019-05-14 15:58:06 -0500

    nod done

---
+++ src/core.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import { car, cdr } from 'hexlet-pairs';
+
+
+const game = (description, intro) => {
+  console.log('Welcome to the Brain Games!');
+  console.log(intro);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!`);
+
+  for (let n = 0; n < 3; n += 1) {
+    const gameCheck = description();
+    const getQuestion = car(gameCheck);
+    const getAnswer = cdr(gameCheck);
+    console.log(`Question: ${getQuestion}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === getAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${userAnswer} is wrong answer. Correct answer is ${getAnswer}\nLet's try again, ${userName}!`);
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default game;
