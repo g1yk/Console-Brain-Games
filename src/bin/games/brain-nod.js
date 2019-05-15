12f530f65dab17e792d57db1a006fea61cd33e84
Vitalii <vitaly.agapov@Outlook.com>
2019-05-14 19:43:21 -0500

    publish done

--- src/bin/games/brain-nod.js
+++ src/bin/games/brain-nod.js
@@ -2,26 +2,26 @@ import { cons } from 'hexlet-pairs';
 import game from '../../core';
 
 const randomInteger = (min, max) => {
-    let rand = min - 0.5 + Math.random() * (max - min + 1)
-    rand = Math.round(rand);
-    return rand;
-  };
-  
-  const description = () => {
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  rand = Math.round(rand);
+  return rand;
+};
+
+const description = () => {
   const num1 = randomInteger(0, 50);
-  const num2 = randomInteger(0, 50)
+  const num2 = randomInteger(0, 50);
   const gcd = (a, b) => {
-      if ( ! b) {
-          return a;
-      }
-      return gcd(b, a % b);
+    if (!b) {
+      return a;
+    }
+    return gcd(b, a % b);
   };
-  const question = `${num1}, ${num2}`
-  const correctAnswer = gcd(num1, num2)
-  
-  return cons(question, correctAnswer.toString())
-  }
-  const intro = 'Find the greatest common divisor of given numbers.';
-  
-  
-  game(description, intro)
+  const question = `${num1}, ${num2}`;
+  const correctAnswer = gcd(num1, num2);
+
+  return cons(question, correctAnswer.toString());
+};
+const intro = 'Find the greatest common divisor of given numbers.';
+
+
+game(description, intro);
